c0c3730759fbbd3439cc2f0dff457c6156aa141c
Jessica Alves <jessica@suasvendas.com>
2024-04-15 12:22:11 -0300

    Include animation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.airbnb.android:lottie:6.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/jessmobilesolutions/tradelink/MainActivity.kt
+++ app/src/main/java/com/jessmobilesolutions/tradelink/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupAnimation()
+    }
+
+    private fun setupAnimation() {
+        val currentNightMode = resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK
+        val isNightMode = currentNightMode == android.content.res.Configuration.UI_MODE_NIGHT_YES
+        val animationView = findViewById<LottieAnimationView>(R.id.animation_view)
+        val animationRes = if (isNightMode) R.raw.animation_night else R.raw.animation_light
+        animationView.setAnimation(animationRes)
     }
 }
