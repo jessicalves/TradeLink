3eb70424b83195a636349e674d87ed5b752a663c
Jessica Alves <jessica@suasvendas.com>
2024-04-15 17:33:28 -0300

    Screen Login

--- app/src/main/java/com/jessmobilesolutions/tradelink/MainActivity.kt
+++ app/src/main/java/com/jessmobilesolutions/tradelink/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jessmobilesolutions.tradelink
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,8 +19,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         setupAnimation()
+        setupView()
     }
 
     private fun setupAnimation() {
@@ -27,5 +29,21 @@ class MainActivity : AppCompatActivity() {
         val animationView = findViewById<LottieAnimationView>(R.id.animation_view)
         val animationRes = if (isNightMode) R.raw.animation_night else R.raw.animation_light
         animationView.setAnimation(animationRes)
+    }
+
+    private fun setupView() {
+        val btnClient = findViewById<Button>(R.id.btnClient)
+        val btnRepresentative = findViewById<Button>(R.id.btnRepresentative)
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        
+        btnClient.setOnClickListener {
+            loginIntent.putExtra("login_type", "client")
+            startActivity(loginIntent)
+        }
+
+        btnRepresentative.setOnClickListener {
+            loginIntent.putExtra("login_type", "representative")
+            startActivity(loginIntent)
+        }
     }
 }
